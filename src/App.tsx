48a815c65bab44d99f635e01ca2efcec7336d66c
Nikita Filippov <filippov.nikita1998@gmail.com>
2022-01-24 11:54:00 -0800

    First step!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    return (
+        <div className="app-wrapper">
+            <header className="header">
+                <img className="img"
+                     src="https://img.search.brave.com/VQtKjHeni0eFuWjVdWdgpfV9UvzMf-0rireieM7yPDU/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5T/MUVlTXE1NEZreXA0/U2daclNXMi1RSGFF/OCZwaWQ9QXBp"
+                     alt=""/>
+            </header>
+            <nav className="nav">
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>https://img.search.brave.com/uA39iedoEahn1BHqZqOWK9AjnQUDk27eUNaOvGNm9bg/rs:fit:844:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5k/cDJNOWdwN2hzWWFP/YmExLTZ2dnB3SGFF/SyZwaWQ9QXBp
+                <div><a>Music</a></div>
+            </nav>
+            <div className="content">
+                <img
+                    src="https://img.search.brave.com/uA39iedoEahn1BHqZqOWK9AjnQUDk27eUNaOvGNm9bg/rs:fit:844:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5k/cDJNOWdwN2hzWWFP/YmExLTZ2dnB3SGFF/SyZwaWQ9QXBp"
+                    alt=""/>
+                <div>Ava + desription</div>
+                <div>My Posts
+                    <div>
+                        New Post
+                    </div>
+                </div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    );
+
+};
 
 export default App;
